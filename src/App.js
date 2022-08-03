0cce461296c8f511e27ff60ffbef13c98fcc3bc2
Abby F <abby_faigao@yahoo.com>
2022-08-02 23:10:55 -0700

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import { Users } from "users.js";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <input type="text" placeholder="Search..." className="search" />
+      <ul>
+        {Users.map((user) => (
+          <li className="listItem">{user.firstname}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
