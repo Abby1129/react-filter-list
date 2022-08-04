9dda8a501aa7efe09c073aab0b6f01d7dba140a7
Abby F <abby_faigao@yahoo.com>
2022-08-04 12:47:19 -0700

    search with tables

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import { useState } from "react";
 import { Users } from "./users.js";
 import "./App.css";
+import Table from "./Table";
 
 function App() {
   const [query, setQuery] = useState("");
+
+  const keys = ["first_name", "last_name", "email"];
+
+  const search = (data) => {
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(query))
+    );
+  };
+
   return (
     <div className="app">
       <input
@@ -12,15 +22,8 @@ function App() {
         className="search"
         onChange={(e) => setQuery(e.target.value)}
       />
-      <ul className="list">
-        {Users.filter((user) =>
-          user.first_name.toLowerCase().includes(query)
-        ).map((user) => (
-          <li className="listItem" key={user.id}>
-            {user.first_name}
-          </li>
-        ))}
-      </ul>
+
+      <Table data={search(Users)} />
     </div>
   );
 }
