ac31c358efd2d0e710b8ea24ce75eb4010f8166d
Abby F <abby_faigao@yahoo.com>
2022-08-05 13:04:18 -0700

    search/fetch data from a simple api

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
-import { useState } from "react";
-import { Users } from "./users.js";
+import { useState, useEffect } from "react";
+
 import "./App.css";
 import Table from "./Table";
+import axios from "axios";
 
 function App() {
   const [query, setQuery] = useState("");
+  const [data, setData] = useState([]);
 
-  const keys = ["first_name", "last_name", "email"];
-
-  const search = (data) => {
-    return data.filter((item) =>
-      keys.some((key) => item[key].toLowerCase().includes(query))
-    );
-  };
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get(`http://localhost:5000?q=${query}`);
+      setData(res.data);
+    };
+    if (query.length === 0 || query.length > 2) fetchData();
+  }, [query]);
 
   return (
     <div className="app">
@@ -20,10 +22,10 @@ function App() {
         type="text"
         placeholder="Search..."
         className="search"
-        onChange={(e) => setQuery(e.target.value)}
+        onChange={(e) => setQuery(e.target.value.toLowerCase())}
       />
 
-      <Table data={search(Users)} />
+      <Table data={data} />
     </div>
   );
 }
